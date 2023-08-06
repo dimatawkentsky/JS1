f8bbfea90179f279c40a4a34890714c8be3fcdff
dimatawkentsky <takugawadima@gmail.com>
2023-08-06 15:20:59 +0500

    js1

---
+++ script.js
@@ -0,0 +1,40 @@
+var modal = prompt ("Ismingizni kiriting")
+console.log("Sizning ismingiz "+modal);
+var modal = prompt( "Yoshingizni kiriting")
+console.log("Sizning yoshingiz "+ modal);
+var modal = prompt( "19+17")
+if (modal == (19+17)) {
+  alert( "Sizning javobiniz to'gri")
+  console.log("Sizning javobiniz to'gri");
+}
+else{
+  alert( "Sizning javobingiz xato; "+ "To'g'ri javob:36"  )
+  console.log("To'g'ri javob: 19+17= "+ (19+17 ) + " Sizning javobingiz:" + modal);
+}
+var modal = prompt( "19-17")
+if (modal == (19-17)) {
+  alert( "Sizning javobiniz to'gri")
+  console.log("Sizning javobiniz to'gri");
+}
+else{
+  alert( "Sizning javobingiz xato; "+ "To'g'ri javob:2"  )
+  console.log("To'g'ri javob: 19-17= "+ (19-17 ) + " Sizning javobingiz:" + modal);
+}
+var modal = prompt( "19*17")
+if (modal == (19*17)) {
+  alert( "Sizning javobiniz to'gri")
+  console.log("Sizning javobiniz to'gri");
+}
+else{
+  alert( "Sizning javobingiz xato; "+ "To'g'ri javob:323"  )
+  console.log("To'g'ri javob: 19*17= "+ (19*17 ) + " Sizning javobingiz:" + modal);
+}
+var modal = prompt( "323/17")
+if (modal == (323/17)) {
+  alert( "Sizning javobiniz to'gri")
+  console.log("Sizning javobiniz to'gri");
+}
+else{
+  alert( "Sizning javobingiz xato; "+ "To'g'ri javob:19"  )
+  console.log("To'g'ri javob: 323/17= "+ (323/17 ) + " Sizning javobingiz:" + modal);
+}
